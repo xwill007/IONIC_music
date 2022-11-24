68ec6ff1a9282b8ea151bb15f15098fbe03d67fa
Ecity-Will <Ecity@Pavilion>
2022-11-24 14:03:07 -0500

    Slides

---
+++ android/app/src/main/java/xwill/music/co/MainActivity.java
@@ -0,0 +1,5 @@
+package xwill.music.co;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'xwill.music.co',
+  appName: 'music',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
